4658d0f515a83a871cc2c14d0a759bbe0c3b0d54
LeoChaeeeun <cheun0327@gmail.com>
2020-01-23 16:19:05 +0900

    2nd
    detail_article, create_article

--- Django_4_THU/article/urls.py
+++ Django_4_THU/article/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from article.views import list_article
+from article.views import list_article, detail_article, create_article
 
 urlpatterns=[
     path('', list_article, name='list_article'),
+    path('detail/<int:pk>/', detail_article, name='detail_article'),
+    path('create/', create_article, name='create_article')
 ]
--- Django_4_THU/article/views.py
+++ Django_4_THU/article/views.py
@@ -8,3 +8,13 @@ def list_article(request):
         'articles': articles
     }
     return render(request, 'list_article.html', data)
+
+def detail_article(request, pk):
+    article=Article.objects.get(pk=pk)
+    data={
+        'article': article
+    }
+    return render(request, 'detail_article.html', data)
+
+def create_article(request):
+    return render(request, 'create_article.html')
